a3065904ad4aa9e3dcedf3a278dd36860d122db9
SebastianCea <se.cea@duocuc.cl>
2025-05-15 09:10:27 -0400

    Initial commit

---
+++ src/test/java/com/example/Proveedores/ProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
